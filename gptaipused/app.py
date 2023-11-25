d73071b2507e1dfe9a02cc132e35c9cd84d750f8
Taehun Lee <thiskorea@cberi.go.kr>
2023-11-26 01:48:00 +0900

    많은 변화...

--- gptaipused/app.py
+++ gptaipused/app.py
@@ -1,16 +1,17 @@
 from flask import Flask, request, jsonify, render_template, send_file
+from datetime import datetime
 import openai
 import os
 import sqlite3 as sq
 import pandas as pd
-import env
+#import env
 
 app = Flask(__name__)
 
 # OpenAI API 키를 환경변수에서 가져옵니다.
 # 환경변수에 'OPENAI_API_KEY'로 저장되어 있어야 합니다.
-# openai.api_key = os.getenv('OPENAI_API_KEY')
-openai.api_key=env.API_KEY
+openai.api_key = os.getenv('OPENAI_API_KEY')
+# openai.api_key=env.API_KEY
 
 #LOCAL DB 연결
 def create_connection():
@@ -23,6 +24,13 @@ conn, c = create_connection()
 
 c.execute('''create table IF NOT EXISTS stuQuestions(id integer PRIMARY KEY AUTOINCREMENT, 
         stuNum text, stuName text, stuAsk TEXT, chatbotAnswer text, answer text)''')
+# 새로운 notices 테이블 생성
+c.execute('''CREATE TABLE IF NOT EXISTS notices (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    date TEXT NOT NULL)''')
+print("Table created successfully")
 c.close() #커서 종료
 conn.close() #커넥션 종료
 
@@ -33,16 +41,18 @@ def ask():
     data = request.json
     kor = '한글로 답해주고, '
     ca = '정확한 답은 알려주지 말고 힌트로 제공해줘.'
+    messages = []
     user_message = kor+data['message']+ca
+    messages.append({"role": "user", "content": f"{user_message}"})
     try:
-        response = openai.ChatCompletion.create(
+        response = openai.chat.completions.create(
             model="gpt-4",  # 코드 해석을 위한 모델을 지정하세요.
-            messages=[{"role": "system", "content": "You are a helpful assistant."},
-                      {"role": "user", "content": user_message}]
+            messages=[{"role": "user", 
+                       "content": user_message,},],
         )
 
         # 응답에서 코드를 추출하고 형식을 잘 구성합니다.
-        code_response = response.choices[0].message['content']
+        code_response = response.choices[0].message.content
         formatted_code = f'Assistant:\n{code_response}\n'  # Markdown 형식으로 코드 블록을 생성합니다.
 
         #db호출   # 민수쌤 코드
@@ -62,7 +72,7 @@ def ask():
         return jsonify({'response': formatted_code}), 200
     except Exception as e:
         app.logger.error(f'An error occurred: {str(e)}')
-        return jsonify({'error': 'An internal error occurred.'}), 500
+        return jsonify({'error': 'An internal error occurred: {str(e)}'}), 500
 
 def get_dataframe_from_db():
     #db호출
@@ -98,21 +108,123 @@ def export():
                      attachment_filename='result.csv',
                      as_attachment=True)
 
+# 공지사항 데이터베이스 관련
+# 1. Create (생성) 
+@app.route('/add', methods=['GET', 'POST'])
+def add_notice():
+    if request.method == 'POST':
+        conn, c = create_connection()
+
+        title = request.form['title']
+        content = request.form['content']
+        #현재 날짜와 시간 설정
+        current_time = datetime.now()
+        formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        now = formatted_time  
+
+        c.execute("INSERT INTO notices (title, content, date) VALUES (?, ?, ?)", 
+                  (title, content, now))
+        conn.commit()
+
+        c.close()
+        conn.close()
+
+        df = get_dataframe_from_notice_db()
+        print(df)
+
+        return render_template('admin.html', table=df.to_html(classes='table table-striped'))
+    else:
+        # GET 요청 처리
+        return render_template('add_notice.html')
+
+# 2. Read (조회)
+@app.route('/notices')
+def show_notices():
+    conn, c = create_connection()
+    c.execute("SELECT * FROM notices")
+    notices = c.fetchall()
+    c.close()
+    conn.close()
+
+    # 이후에는 'notices' 변수를 사용하여 템플릿에 데이터를 전달합니다.
+    return render_template('notices.html', notices=notices)
+
+# 3. Update (수정)
+@app.route('/update/<int:id>', methods=['POST'])
+def update_notice(id):
+    conn, c = create_connection()
+
+    title = request.form['title']
+    content = request.form['content']
+
+    c.execute("UPDATE notices SET title = ?, content = ? WHERE id = ?", 
+              (title, content, id))
+    conn.commit()
+
+    c.close()
+    conn.close()
+
+    df = get_dataframe_from_notice_db()
+    print(df)
+
+    return render_template('admin.html', table=df.to_html(classes='table table-striped'))
+# 4. Delete(삭제)
+@app.route('/delete', methods=['POST'])
+def delete_notice():
+    notice_id = request.form.get('noticeId')
+    if notice_id:
+        conn, c = create_connection()
+
+        c.execute("DELETE FROM notices WHERE id = ?", (notice_id,))
+        conn.commit()
+
+        c.close()
+        conn.close()
+
+        df = get_dataframe_from_notice_db()
+        print(df)
+
+        return render_template('admin.html', table=df.to_html(classes='table table-striped'))
+    else:
+        return "There is no deleted notice"
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route('/info')
-def info():
-    return render_template('01info.html')
+def get_dataframe_from_notice_db():
+    #db호출
+    conn, c = create_connection()
+    query = "SELECT * FROM notices"
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
 
-@app.route('/ai')
-def ai():
-    return render_template('02ai.html')
+@app.route("/main")
+def main():
+    df = get_dataframe_from_notice_db()
+    print(df)
 
-@app.route('/exercise')
-def exercise():
-    return render_template('03exer.html')
+    return render_template('main.html', table=df.to_html(classes='table table-striped'))
+
+@app.route("/admin")
+def admin():
+    df = get_dataframe_from_notice_db()
+    print(df)
+
+    return render_template('admin.html', table=df.to_html(classes='table table-striped'))
+
+@app.route('/nav1')
+def nav1():
+    return render_template('nav1.html')
+
+@app.route('/nav2')
+def nav2():
+    return render_template('nav2.html')
+
+@app.route('/nav3')
+def nav3():
+    return render_template('nav3.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
